4cd9059a4b0bd121b95fac23abf43f06020df5d3
Michelle Sun <michelle.sun032@icloud.com>
2022-01-31 22:45:39 +1100

    Added a mutually exclusive decorator

--- mdtedit/utils.py
+++ mdtedit/utils.py
@@ -1,30 +1,57 @@
+import functools
+import os
+import time
 
+from typing import Callable, List
 
 def colored(r: int, g: int, b: int, text: str) -> str:
     """Returns a colored version of the string."""
     # return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
     return "\033[38;2;{};{};{}m{}\033[m".format(r, g, b, text)
 
+# TODO: Decorator type hints????? 
+def mutually_exclusive(keyword, *keywords):
+    keywords = (keyword,)+keywords
+    def _wrapper(func):
+        @functools.wraps(func)
+        def inner(*args, **kwargs):
+            if sum(k in keywords for k in kwargs) != 1:
+                raise TypeError('You must specify exactly one of {}'.format(', '.join(keywords)))
+            return func(*args, **kwargs)
+        return inner
+    return _wrapper
 
-# def main():
-#     import re
-#     import os
+def timer(function_without_args=None, *args, **kwargs):
+    """Decorator function that times the functions."""
+    # print(type(func_no_args), len(args), len(kwargs))
+    precision = {
+        's': 1,
+        'ms': 1000,
+    }
+    # Default args
+    unit = 'ms'
+    rnd = 0
     
-#     sample = "directory/subdirectory/file-name-123.txt"
-#     search = "-"
-#     replace = "_"
+    def _decorate(func):
+        
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            diff = (end-start) * precision[unit]
+            if rnd:
+                diff = round(diff,rnd)
+            print(f'\'{func.__name__}\' finished in {diff} {unit}')
+            return result
+        return wrapper
     
-#     head, tail = os.path.split(sample)
-#     tail, ext = os.path.splitext(tail)
+    if function_without_args:
+        return _decorate(function_without_args)
     
-#     print(os.path.join(head, tail+ext))
+    if 'unit' in kwargs:
+        unit = kwargs['unit']
+    if 'round' in kwargs:
+        rnd = kwargs['round']
     
-#     matches = re.findall(search, sample)
-#     for match in matches:
-#         print(match)
-#     colored_text = re.sub(search, lambda m: colored(255,0,0,m.group()) + colored(0,255,0,replace), sample)
-#     print(colored_text)
-
-
-# if __name__ == "__main__":
-#     main()
+    return _decorate
