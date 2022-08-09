452f91fd622c4322f8e2d5157f6f3a21f52979ec
Michelle Sun <michelle.sun032@icloud.com>
2022-08-10 02:40:13 +1000

    Removed deprecated tests.

--- tests/test_pyreutil.py
+++ tests/test_pyreutil.py
@@ -18,45 +18,6 @@ class TestReUtil(unittest.TestCase):
         result2 = ReUtil().search_and_replace(regex_with_groups, sample_text, replace="[link]")
         self.assertEquals(expected1, result1)
         self.assertEquals(expected2, result2)
-        
-    # TODO
-    def test_search(self):
-        pass
-    
-    # TODO
-    def test_remove(self):
-        pass
-
-
-class TestText(unittest.TestCase):
-    
-    def test_init(self):
-        filename_sample = ['examples/sample1.md', 'examples/sample2.md', 'examples/sample3.txt']
-        text_sample = [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis molestie quam, eu sodales lectus.",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a dolor elit. Cras pellentesque erat eros, eu volutpat lacus fermentum id. Integer commodo dui ut placerat.",
-            "Donec sed blandit nisl. Aenean blandit erat sit amet urna viverra hendrerit. Sed luctus nulla sit."
-        ]
-        self.assertRaises(TypeError, Text, text=text_sample, filenames=filename_sample)
-        pass
-    
-    # TODO
-    def test_strip_markdown_links(self):
-        pass
-    
-    # TODO
-    def test_remove_extra_whitespaces(self):
-        pass
-
-
-class TestPathnames(unittest.TestCase):
-    
-    # TODO
-    def test_init(self):
-        sample_pathnames = ['examples/sample1.md', 'examples/sample2.md', 'examples/sample3.txt']
-        
-        self.assertRaises(TypeError, Pathnames, path='doesnotexist.txt', pathnames=sample_pathnames)
-        pass
    
 if __name__ == "__main__":
     unittest.main()
